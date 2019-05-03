f1be58be21a0214ba94f251509befdca4fd7097e
David Hewson <david.hewson@tracsis.com>
2019-05-03 22:18:10 +0100

    support small arrays and CONSTR_NOCAF in ghc-heap

--- libraries/ghc-heap/GHC/Exts/Heap/Closures.hs
+++ libraries/ghc-heap/GHC/Exts/Heap/Closures.hs
@@ -211,6 +211,13 @@ data GenClosure b
         -- Card table ignored
         }
 
+    -- | A @SmallMutableArray#@
+  | SmallMutArrClosure
+        { info       :: !StgInfoTable
+        , mccPtrs    :: !Word           -- ^ Number of pointers
+        , mccPayload :: ![b]            -- ^ Array payload
+        }
+
     -- | An @MVar#@, with a queue of thread state objects blocking on them
   | MVarClosure
         { info       :: !StgInfoTable
@@ -311,6 +318,7 @@ allClosures (APStackClosure {..}) = fun:payload
 allClosures (BCOClosure {..}) = [instrs,literals,bcoptrs]
 allClosures (ArrWordsClosure {..}) = []
 allClosures (MutArrClosure {..}) = mccPayload
+allClosures (SmallMutArrClosure {..}) = mccPayload
 allClosures (MutVarClosure {..}) = [var]
 allClosures (MVarClosure {..}) = [queueHead,queueTail,value]
 allClosures (FunClosure {..}) = ptrArgs
